8e12c9e1438bc7d400a569a82e537d36ff645d2e
Stephen Wang <ste53539@gmail.com>
2023-02-01 03:59:03 +0000

    added color driver and new imu driver

--- src/robot/scripts/gyro_icm.py
+++ src/robot/scripts/gyro_icm.py
@@ -2,6 +2,7 @@
 import rospy
 import tf
 import utils
+import numpy as np
 from icm20948 import ICM20948
 from geometry_msgs.msg import Accel, Vector3, Twist
 from nav_msgs.msg import Odometry
@@ -21,16 +22,30 @@ msgs = utils.create_msgs(topics)
 msgs["/odom"].header.frame_id = "world"
 msgs["/odom"].child_frame_id = "robot"
 
+position = np.array([0.]*3) # initialize robot position
+velocity = np.array([0.]*3)
+angle = np.array([0.]*3)
+
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
   ax, ay, az, gx, gy, gz = imu.read_accelerometer_gyro_data()
+  a = np.array([ax, ay, az])
+  omega = np.array([gx, gy, gz])
 
-  quat = tf.transformations.quaternion_from_euler(gx, gy, gz)
+  # numerical integration
+  velocity += a * 0.1
+  position += velocity * 0.1
+  angle += omega * 0.1
+
+  quat = tf.transformations.quaternion_from_euler(*angle)
   utils.assign_quat(msgs["/odom"].pose.pose.orientation, quat)
+  utils.assign_vec(msgs["/odom"].pose.pose.position, *position)
+  utils.assign_vec(msgs["/odom"].twist.twist.linear, *velocity)
+  utils.assign_vec(msgs["/odom"].twist.twist.angular, *omega)
+  msgs["/odom"].header.stamp = rospy.Time.now()
 
   utils.assign_vec(msgs["/raw/accel"].linear, ax, ay, az)
 
-  # TODO: numerically integrate linear accel to get linear twist (vel)
   utils.assign_vec(msgs["/raw/gyro"].angular, gx, gy, gz)
 
   utils.publish_all(pubs, msgs)
---
+++ src/robot/scripts/viz_color.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import rospy
+import tkinter as tk
+
+window = tk.Tk()
+
+button = tk.
+greeting.pack()
+
+window.mainloop()
