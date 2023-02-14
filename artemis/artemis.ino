1841c62834a4083ca659a006a82a5d83f861fe0c
Stephen Wang <ste53539@gmail.com>
2023-02-13 23:09:13 -0500

    updated main sketch

--- artemis/artemis.ino
+++ artemis/artemis.ino
@@ -1,22 +1,28 @@
 #include <Wire.h>
 #include "tof.h"
+#include "imu.h"
 
+#define I2C_FREQ 400000
+
+TwoWire i2c(D25, D27);
 ToF tof;
+IMU imu;
 
 void setup() {
 
   Serial.begin(115200);
   while(!Serial);
 
-  Wire.begin();
-  Wire.setClock(400000); // 400 kHz
+  i2c.begin();
+  i2c.setClock(I2C_FREQ);
 
   tof.begin();
+  imu.begin(i2c, I2C_FREQ);
 }
 
 void loop() {
   tof.read();
-  Serial.print(tof.get_data()->distance_mm[3]);
+  Serial.print(tof.data.distance_mm[3]);
   Serial.println();
   delay(3);
 }
