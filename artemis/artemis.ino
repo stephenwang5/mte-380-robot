fe07d4bc57c74a567629c10851e0830fedd31f31
jramseye <jramseye@uwaterloo.ca>
2023-02-16 19:02:34 -0500

    fixed tof driver

--- artemis/artemis.ino
+++ artemis/artemis.ino
@@ -6,8 +6,8 @@
 
 #define I2C_FREQ 400000
 
-const int robotWidth = 4; // robot width in inches
-const int wheelRadius = 3; // wheel radius in inches
+const int robotWidth = 4.5; // robot width in inches
+const int wheelRadius = 3.25; // wheel radius in inches
 #define TICKS_PER_REV 2100 // ticks per revolution of the encoder
 
 Motor L_Motor = Motor(35,6,Encoder(14)); //bridged pin 6 to pin 34 on board because pin 34 wouldnt output a PWM
@@ -26,8 +26,8 @@ static void LencoderUpdateISR() {
 /********** ISRs **********/
 
 TwoWire i2c(D25, D27);
-ToF tof;
-IMU imu;
+SparkFun_VL53L5CX tof;
+MPU9250 imu = MPU9250(MPU9250_ADDRESS_AD0, i2c, I2C_FREQ);
 
 void setup() {
 
@@ -35,15 +35,17 @@ void setup() {
   while(!Serial);
   pinMode(34, INPUT); // set broken motor pin to input so it doesnt interfere with pwm at now at pin 6
 
+  R_Motor.begin();
+  L_Motor.begin();
+
   attachInterrupt(digitalPinToInterrupt(L_Motor.encoder.pinA), RencoderUpdateISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(R_Motor.encoder.pinA), LencoderUpdateISR, CHANGE);
 
   i2c.begin();
   i2c.setClock(I2C_FREQ);
 
-  tof.begin();
-  imu.begin(i2c, I2C_FREQ);
-
+  //ToF_begin(tof);
+  init_imu(imu);
 }
 
 void forward(uint8_t pwm){
@@ -74,7 +76,7 @@ void TurnInPlace(int numDegrees) {
   int clockwise = numDegrees > 0;
   int pwm = 100;
 
-  float turnDist = PI*12*(abs(numDegrees))/360.0f; // turn distance of each wheel
+  float turnDist = PI*(robotWidth/2)*(abs(numDegrees))/360.0f; // turn distance of each wheel
   uint16_t numticks = uint16_t((turnDist*TICKS_PER_REV)/(PI*2*wheelRadius)); // number of encoder ticks needed to turn provided number of degrees
 
   int R_startPos = R_Motor.encoder.encoderPos;
@@ -101,22 +103,78 @@ void TurnInPlace(int numDegrees) {
   }
 }
 
+void TurntoMagPos(int magX, int magY){
+
+  read_imu(imu);
+  int currentMagX = imu.mx;
+  int currentMagY = imu.my;
+  int pwm = 50;
+  R_Motor.clockwise(pwm);
+  L_Motor.clockwise(pwm);
+  while((currentMagX > 1.05*magX || currentMagX < 0.95*magX) && (currentMagY > 1.05*magY || currentMagY < 0.95*magY)){
+    read_imu(imu);
+    currentMagX = imu.mx;
+    currentMagY = imu.my;
+  }
+
+  coast();
+}
+
+void TurntoYawPos(int yaw) {
+  read_imu(imu);
+
+  int currentYaw = imu.yaw;
+  int pwm = 50;
+  R_Motor.clockwise(pwm);
+  L_Motor.clockwise(pwm);
+
+  while((currentYaw > 1.05*yaw || currentYaw < 0.95*yaw)){
+    read_imu(imu);
+    currentYaw = imu.yaw;
+  }
+
+  coast();
+}
+
 void loop() {
-  tof.read();
-  Serial.print(tof.data.distance_mm[3]);
-  Serial.println();
-  delay(3);
-  // // put your main code here, to run repeatedly:
-  // TurnInPlace(-90);
-  // delay(2000);
+
+  // read_imu(imu);
+  // int yaw = imu.yaw;
+  // Serial.print("Starting yaw: ");
+  // Serial.println(yaw);
+  // Serial.println("change robot orientation");
+  // delay(5000);
+  // TurntoYawPos(yaw);
+  // Serial.println("robot should be at same start position");
+
+  //tof.read();
+  // Serial.print(tof.data.distance_mm[3]);
+  // Serial.println();
+  // delay(3);
+
+  //IMU
+  // imu.read();
+  // Serial.print("Mx: ");
+  // Serial.println(imu.sensor.mx);
+  // Serial.print("My: ");
+  // Serial.println(imu.sensor.my);
+  // Serial.print("Mz: ");
+  // Serial.println(imu.sensor.mz);
+  // delay(50);
+  
+  // put your main code here, to run repeatedly:
+  TurnInPlace(-90);
+  delay(2000);
+  TurnInPlace(90);
+  delay(4000);
   // forward(100);
-  // delay(2000);
-  // coast();
-  // delay(2000);
-  // TurnInPlace(90);
-  // delay(2000);
-  // backward(100);
-  // delay(2000);
-  // coast();
-  // delay(4000);
+  delay(2000);
+  coast();
+  delay(2000);
+  TurnInPlace(90);
+  delay(2000);
+  backward(100);
+  delay(2000);
+  coast();
+  delay(4000);
 }
--- artemis/tof.h
+++ artemis/tof.h
@@ -3,14 +3,41 @@
 
 #include <SparkFun_VL53L5CX_Library.h>
 
-class ToF {
-public:
-  SparkFun_VL53L5CX sensor;
-  VL53L5CX_ResultsData data;
+VL53L5CX_ResultsData ToF_data;
 
-  ToF();
-  void begin();
-  void read();
-};
+void ToF_begin(SparkFun_VL53L5CX& sensor) {
+  if (!sensor.begin()) {
+    Serial.println("tough luck. tof not found");
+    while (1);
+  }
+
+#ifdef DEBUG
+  Serial.println("upload complete!");
+#endif
+
+  sensor.setResolution(64);
+
+  sensor.setRangingMode(SF_VL53L5CX_RANGING_MODE::CONTINUOUS);
+  sensor.setRangingFrequency(15);
+
+  sensor.startRanging();
+}
+
+void ToF_read(SparkFun_VL53L5CX& sensor) {
+  if (sensor.isDataReady()) {
+    if (sensor.getRangingData(&ToF_data)) {
+
+#ifdef DEBUG
+      Serial.print(millis() - start_time);
+      Serial.print(" ms ");
+      start_time = millis();
+      Serial.println(" new data available");
+#endif
+
+    } else {
+      Serial.println("yikes tof fetch failed");
+    }
+  }
+}
 
 #endif // TOF_H
