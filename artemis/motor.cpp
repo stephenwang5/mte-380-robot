911c3eb13b1d88835a8a689203109a0685abb5ce
Stephen Wang <ste53539@gmail.com>
2023-02-20 13:36:51 -0500

    merged encoder into motor

--- artemis/motor.cpp
+++ artemis/motor.cpp
@@ -1,25 +1,33 @@
 #include "motor.h"
+#include "main.h"
+
+Motor::Motor(PinName a, PinName b, PinName enc): pinA(a), pinB(b), encoderPin(enc) {}
 
 void Motor::begin() {
-  pinMode(PinA, OUTPUT);
-  pinMode(PinB, OUTPUT);
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
 }
 
-void Motor::clockwise(uint8_t pwm) {
-  analogWrite(PinA, pwm);
-  analogWrite(PinB, 0);
+void Motor::rotateCW(uint8_t pwm) {
+  analogWrite(pinA, pwm);
+  analogWrite(pinB, 0);
   direction = CW;
 }
 
-void Motor::counterclockwise(uint8_t pwm) {
-  analogWrite(PinA, 0);
-  analogWrite(PinB, pwm);
+void Motor::rotateCCW(uint8_t pwm) {
+  analogWrite(pinA, 0);
+  analogWrite(pinB, pwm);
   direction = CCW;
 }
 
 void Motor::coast() {
-  analogWrite(PinA, 0);
-  analogWrite(PinB, 0);
+  analogWrite(pinA, 0);
+  analogWrite(pinB, 0);
+}
+
+void Motor::activeBreak() {
+  analogWrite(pinA, 255);
+  analogWrite(pinB, 255);
 }
 
 // uint8_t Motor::controlSpeed() {
@@ -33,6 +41,52 @@ void Motor::coast() {
 //   return Output;
 // }
 
-void Motor::setSetpoint(uint8_t setpoint){
-  Setpoint = setpoint;
+// void Motor::setSetpoint(uint8_t setpoint){
+//   Setpoint = setpoint;
+// }
+
+void Motor::encoderUpdate() {
+  encoder += direction;
+}
+
+void Motor::calculateSpeed() {
+  int32_t curTime = micros();
+  float duration = (curTime - prevTime) * 1e-6f;
+  speed = (float)(encoder - prevEncoder) / TICKS_PER_REV / duration;
+
+  prevTime = curTime;
+  prevEncoder = encoder;
+}
+
+static void updateRightEncoder() {
+  rightMotor.encoderUpdate();
+}
+
+static void updateLeftEncoder() {
+  leftMotor.encoderUpdate();
+}
+
+void registerEncoderISRs() {
+  attachInterrupt(digitalPinToInterrupt(leftMotor.encoderPin), updateRightEncoder, RISING);
+  attachInterrupt(digitalPinToInterrupt(rightMotor.encoderPin), updateLeftEncoder, RISING);
+}
+
+void forward(uint8_t pwm){
+  leftMotor.rotateCCW(pwm);
+  rightMotor.rotateCW(pwm);
+}
+
+void backward(uint8_t pwm){
+  leftMotor.rotateCW(pwm);
+  rightMotor.rotateCCW(pwm);
+}
+
+void coast(){
+  leftMotor.coast();
+  rightMotor.coast();
+}
+
+void activeBreak() {
+  leftMotor.activeBreak();
+  rightMotor.activeBreak();
 }
--- artemis/motor.h
+++ artemis/motor.h
@@ -1,33 +1,47 @@
 #ifndef MOTOR_H
 #define MOTOR_H
-#include "encoder.h"
+
+enum Direction {
+  CW = 1,
+  CCW = -1,
+};
 
 class Motor{
-  public:
-    Motor(int a, int b, Encoder enc): PinA(a), PinB(b), encoder(enc) {
-      
-      // pidController = PID(&(encoder.encoderSpeed), &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-      // pidController.SetMode(AUTOMATIC);
-      // pidController.SetSampleTime(100);
-    }
+public:
 
-    Encoder encoder;
-    const int PinA;
-    const int PinB;
-    int direction;
-    //PID pidController;
+  int64_t encoder;
+  int64_t prevEncoder;
+  int32_t prevTime;
+  PinName encoderPin;
+  float speed;
 
-    double Setpoint, Input, Output;
-    double lastEncoderCount = 0;
-    double Kp=1, Ki=0.1, Kd=0.01;
+  const PinName pinA;
+  const PinName pinB;
+  Direction direction;
 
-    void begin();
-    void clockwise(uint8_t);
-    void coast();
-    void counterclockwise(uint8_t);
-    //void controlSpeed();
+  // TODO: if the PID gain terms aren't dynamic, make them constexpr
+  // double Kp=1, Ki=0.1, Kd=0.01;
+  // double Setpoint, Input, Output;
+  // double lastEncoderCount = 0;
+  // PID pidController;
 
-    void setSetpoint(uint8_t setpoint);
+  Motor(PinName a, PinName b, PinName enc);
+  void begin();
+  void rotateCW(uint8_t);
+  void rotateCCW(uint8_t);
+  void coast();
+  void activeBreak();
+  //void controlSpeed();
+
+  void setSetpoint(uint8_t setpoint);
+
+  void encoderUpdate();
+  void calculateSpeed();
 };
+
+void forward(uint8_t);
+void backward(uint8_t);
+void coast();
+void activeBreak();
 
 #endif // MOTOR_H
