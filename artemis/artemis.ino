a9533f1d1dace190e3f95c4e0c84c58ddce72a26
Stephen Wang <ste53539@gmail.com>
2023-02-24 21:02:14 -0500

    implemented cmsis rtos2 timers but high freq timers cause queue overflow

--- artemis/artemis.ino
+++ artemis/artemis.ino
@@ -1,122 +1,103 @@
-#include "motor.h"
-#include "encoder.h"
-#include <Wire.h>
-#include "tof.h"
-#include "imu.h"
+#include "main.h"
 
-#define I2C_FREQ 400000
+using namespace std::chrono_literals;
 
-const int robotWidth = 4; // robot width in inches
-const int wheelRadius = 3; // wheel radius in inches
-#define TICKS_PER_REV 2100 // ticks per revolution of the encoder
-
-Motor L_Motor = Motor(35,6,Encoder(14)); //bridged pin 6 to pin 34 on board because pin 34 wouldnt output a PWM
-Motor R_Motor = Motor(29,11,Encoder(23));
-
-/********** ISRs **********/
-
-static void RencoderUpdateISR() {
-  R_Motor.encoder.encoderUpdate(R_Motor.direction);
-}
-
-static void LencoderUpdateISR() {
-  L_Motor.encoder.encoderUpdate(L_Motor.direction);
-}
-
-/********** ISRs **********/
+//bridged pin 6 to pin 34 on board because pin 34 wouldnt output a PWM
+Motor leftMotor = Motor(D35, D6, D14);
+Motor rightMotor = Motor(D29, D11, D23);
 
 TwoWire i2c(D25, D27);
-ToF tof;
-IMU imu;
+// ToF tof;
+// MPU9250 imu(MPU9250_ADDRESS_AD0, i2c, I2C_FREQ);
+
+enum ThrowBotState {
+  IDLE,
+  READY,
+  SURVEY,
+  DRIVE,
+} throwbotState; // define states and initialize the state variable
+
+rtos::Thread motorSpeedTask;
+void calculateMotorSpeeds();
+rtos::Thread motorControlTask;
+void controlMotor();
+rtos::Timer tofInputTask;
+rtos::Timer imuInputTask;
+// correct path drift due to wheel slip using odometry estimation
+rtos::Thread pathPlanningTask;
+void courseAdjustment();
+void motorDemo();
 
 void setup() {
 
   Serial.begin(115200);
   while(!Serial);
-  pinMode(34, INPUT); // set broken motor pin to input so it doesnt interfere with pwm at now at pin 6
+  Serial.println("firmware start!");
 
-  attachInterrupt(digitalPinToInterrupt(L_Motor.encoder.pinA), RencoderUpdateISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(R_Motor.encoder.pinA), LencoderUpdateISR, CHANGE);
+  // set broken motor pin to high impedance to stop interference with D6
+  pinMode(34, INPUT);
 
   i2c.begin();
   i2c.setClock(I2C_FREQ);
 
-  tof.begin();
-  imu.begin(i2c, I2C_FREQ);
+  // tof.begin();
+  // initIMU();
 
-}
+  leftMotor.begin();
+  rightMotor.begin();
+  registerEncoderISRs();
 
-void forward(uint8_t pwm){
-  // Left wheel needs to go CCW
-  L_Motor.counterclockwise(pwm);
+  throwbotState = IDLE;
 
-  // Right wheel needs to go CW
-  R_Motor.clockwise(pwm);
+  motorSpeedTask.start(calculateMotorSpeeds);
+  motorControlTask.start(controlMotor);
 
-}
+  Serial.println("left,right");
 
-void backward(uint8_t pwm){
-  // Left wheel needs to go CW
-  L_Motor.clockwise(pwm);
-
-  // Right wheel needs to go CCW
-  R_Motor.counterclockwise(pwm);
-
-}
-
-void coast(){
-  L_Motor.coast();
-  R_Motor.coast();
-}
-
-void TurnInPlace(int numDegrees) {
-
-  int clockwise = numDegrees > 0;
-  int pwm = 100;
-
-  float turnDist = PI*12*(abs(numDegrees))/360.0f; // turn distance of each wheel
-  uint16_t numticks = uint16_t((turnDist*TICKS_PER_REV)/(PI*2*wheelRadius)); // number of encoder ticks needed to turn provided number of degrees
-
-  int R_startPos = R_Motor.encoder.encoderPos;
-  int L_startPos = L_Motor.encoder.encoderPos;
-
-  if (clockwise) { // Left wheel goes forward, right wheel goes back => both wheels spin CCW
-    R_Motor.counterclockwise(pwm);
-    L_Motor.counterclockwise(pwm);
-
-  } else { // Right wheel goes forward, left wheel goes back => both wheels spin CW
-    R_Motor.clockwise(pwm);
-    L_Motor.clockwise(pwm);
-  }
-  int Rturning  = 1; int Lturning = 1;
-  while (Rturning || Lturning) {
-    if (abs(R_Motor.encoder.encoderPos - R_startPos) >= numticks) {
-      R_Motor.coast();
-      Rturning = 0;
-    }
-    if (abs(L_Motor.encoder.encoderPos - L_startPos) >= numticks) {
-      L_Motor.coast();
-      Lturning = 0;
-    }
-  }
 }
 
 void loop() {
-  tof.read();
-  Serial.print(tof.data.distance_mm[3]);
-  Serial.println();
-  delay(3);
-  // // put your main code here, to run repeatedly:
-  // TurnInPlace(-90);
-  // delay(2000);
-  // forward(100);
-  // delay(2000);
-  // coast();
-  // delay(2000);
-  // TurnInPlace(90);
-  // delay(2000);
-  // backward(100);
-  // delay(2000);
-  // coast();
-  // delay(4000);
+
+  Serial.print(leftMotor.encoder);
+  Serial.print(",");
+  Serial.println(rightMotor.encoder);
+  delay(100);
+
+}
+
+void calculateMotorSpeeds() {
+  while (1) {
+    leftMotor.calculateSpeed();
+    rightMotor.calculateSpeed();
+    rtos::ThisThread::sleep_for(1ms);
+  }
+}
+
+void controlMotor() {
+  static uint8_t pwm = 0;
+  while (1) {
+    if (!pwm) {
+      leftMotor.encoder = 0;
+      rightMotor.encoder = 0;
+    }
+    leftMotor.rotateCCW(pwm >> 2);
+    rightMotor.rotateCW(pwm++ >> 2);
+    rtos::ThisThread::sleep_for(10ms);
+  }
+  
+}
+
+void motorDemo() {
+  turnInPlace(-90);
+  delay(2000);
+  forward(100);
+  delay(2000);
+  coast();
+  delay(2000);
+  turnInPlace(90);
+  delay(2000);
+  backward(100);
+  delay(2000);
+  coast();
+  delay(4000);
 }
