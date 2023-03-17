37b7c54671b5e7aff1b0714f1cb206a010ded320
Stephen Wang <ste53539@gmail.com>
2023-03-17 16:50:11 -0400

    added spinning, which is a good trick

--- artemis/control.cpp
+++ artemis/control.cpp
@@ -2,3 +2,22 @@
 
 #include "main.h"
 
+void spinCCW(uint8_t pwm) {
+  if (orientation == IMU_FACE_UP) {
+    leftMotor.rotateCW(pwm);
+    rightMotor.rotateCW(pwm);
+  } else {
+    leftMotor.rotateCCW(pwm);
+    rightMotor.rotateCCW(pwm);
+  }
+}
+
+void spinCW(uint8_t pwm) {
+  if (orientation == IMU_FACE_UP) {
+    leftMotor.rotateCCW(pwm);
+    rightMotor.rotateCCW(pwm);
+  } else {
+    leftMotor.rotateCW(pwm);
+    rightMotor.rotateCW(pwm);
+  }
+}
--- artemis/control.h
+++ artemis/control.h
@@ -1,6 +1,7 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 
-void courseAdjustment();
+void spinCCW(uint8_t pwm);
+void spinCW(uint8_t pwm);
 
 #endif // CONTROL_H
